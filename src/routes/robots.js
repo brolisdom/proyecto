6f749abcd6490db76f2d7c35e1c333cb34652244
Alan Delgado <alan_id_19@hotmail.com>
2020-08-28 13:02:44 -0500

    PDF semi-desing

--- src/routes/robots.js
+++ src/routes/robots.js
@@ -19,23 +19,23 @@ router.post('/voucher', isAuth, async(req, res) => {
         <title>Document</title>
     </head>
     <body style="font-family: Open Sans, Arial;">
-        <div class="container" style="background: #F6F6F6; width: 700px; display: block; margin: auto; border-bottom-left-radius: 70px;">
-            <div class="header" style="background: #5D2ABC; display: block; width: 700px; margin: auto; margin-top: 20px; position: relative;">
-                <h3 style="display: block; width: 90%; margin: auto; text-align: center; padding-bottom: 20px; padding-top: 20px; color: white; font-weight: 400; text-transform: uppercase; font-size: 16px;"> Rama Estidiantil IEEE UPIITA-IPN | Instituto Polit&eacute;cnico Nacional</h3>
-                <h3 style="display: block; width: 65%; margin: auto; text-align: center; color: white; font-weight: 400; text-transform: uppercase; font-size: 40px;"> GUERRA DE ROBOTS</h3>
-                <h3 style="display: block; width: 70%; margin: auto; text-align: center; padding-bottom: 20px; color: white; font-weight: 400; text-transform: uppercase; font-size: 16px;"> El torneo de r&oacute;botica internacional m&aacute;s importante de m&eacute;xico</h3>
+        <div class="container" style="background: #F6F6F6; width: 700px; display: block;">
+            <div class="header" style="background: #5D2ABC; display: block; width: 700px;">
+                <h3 style="display: block; width: 90%;  padding-bottom: 20px; padding-top: 20px; color: white; font-weight: 400; text-transform: uppercase; font-size: 16px;"> Rama Estidiantil IEEE UPIITA-IPN | Instituto Polit&eacute;cnico Nacional</h3>
+                <h3 style="display: block; width: 65%;  color: white; font-weight: 400; text-transform: uppercase; font-size: 40px;"> GUERRA DE ROBOTS</h3>
+                <h3 style="display: block; width: 70%;  padding-bottom: 20px; color: white; font-weight: 400; text-transform: uppercase; font-size: 16px;"> El torneo de r&oacute;botica internacional m&aacute;s importante de m&eacute;xico</h3>
             </div>
-            <div class="content" style="width: 700px; display: block; margin: auto; margin-top: 40px; margin-bottom: 40px;">
-                <h3 style="font-weight: 600; text-align: center; color: #9E7FD7; font-size: 40px; margin-bottom: 10px;">correo</h3>
-                <h3 style="font-weight: 600; text-align: center; color: #9E7FD7; font-size: 40px; margin-bottom: 10px;">Password: </h3>
-                <h4 class="welcome" style="font-weight: 500; margin-bottom: 20px; font-size: 26px; color: #2E155E; text-align: center;"> Gracias por registrarte y se bienvenid&#64; a este gran evento</h4>
-                <h4 style="text-align: center; font-weight: 400; font-size: 20px; color: #000000;"> Para continuar con esta aventura porfavor verifica tu correo electronico haciendo click en el siguiente botón</h4>
-                <a href="" class="button" style="display: block; width: 200px; text-align: center; height: 50px; margin: auto; margin-top: 30px; margin-bottom: 30px; background: #5D2ABC; line-height: 45px; text-decoration: none; color: #DFD4F2; font-size: 16px; border-bottom-left-radius: 20px;"> Verifica mi cuenta</a>
-                <h4 style="text-align: center; font-weight: 400; font-size: 20px; color: #000000;">O copia y pega el siguiente link en el navegador para confirmar tu cuenta</h4>
-                <a class="link" style="margin: auto; width: 70%; display: block; text-align: center; color: #5D2ABC;font-size: 25px;"></a><br>
-                <h4 style="text-align: center; font-weight: 400; font-size: 20px; color: #000000;">Tu contraseña ha sido encriptada y guardada en nuestra base de datos, conserva este correo en caso de que la llegues a olvidar</h4>
+            <div class="content" style="width: 700px; display: block;">
+                <h3 style="font-weight: 600; color: #9E7FD7; font-size: 40px; ">correo</h3>
+                <h3 style="font-weight: 600; color: #9E7FD7; font-size: 40px; ">Password: </h3>
+                <h4 class="welcome" style="font-weight: 500; font-size: 26px; color: #2E155E; "> Gracias por registrarte y se bienvenid&#64; a este gran evento</h4>
+                <h4 style="font-weight: 400; font-size: 20px; color: #000000;"> Para continuar con esta aventura porfavor verifica tu correo electronico haciendo click en el siguiente botón</h4>
+                <a href="" class="button" style="display: block; width: 200px; height: 50px; background: #5D2ABC; line-height: 45px; text-decoration: none; color: #DFD4F2; font-size: 16px; border-bottom-left-radius: 20px;"> Verifica mi cuenta</a>
+                <h4 style="font-weight: 400; font-size: 20px; color: #000000;">O copia y pega el siguiente link en el navegador para confirmar tu cuenta</h4>
+                <a class="link" style="width: 70%; display: block; color: #5D2ABC;font-size: 25px;"></a><br>
+                <h4 style="font-weight: 400; font-size: 20px; color: #000000;">Tu contraseña ha sido encriptada y guardada en nuestra base de datos, conserva este correo en caso de que la llegues a olvidar</h4>
             </div>
-            <div class="footer" style="background: #2E155E; color: #DFD4F2; text-align: center; padding: 20px; margin-bottom: 20px; border-bottom-left-radius: 70px;">
+            <div class="footer" style="background: #2E155E; color: #DFD4F2; ">
                 <h5 class="info" style="font-weight: 100; font-size: 16px;">Si no reconoces esta solicitud o tu no la has hecho, ignora este correo y porfavor ponte en contacto con nosotros: <b>correo@guerraderobots.mx</b></h5>
                 <h5>Guerra de Robots | &copy; 2020 Rama Estudiantil IEEE UPIITA-IPN</h5><h5><a href="#" style="text-decoration: none; color: #DFD4F2;">www.guerraderobots.mx</a></h5>
             </div>
